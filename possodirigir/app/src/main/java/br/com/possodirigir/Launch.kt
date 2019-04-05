9aca5d52ecd8f3c235590e356e8595c29bc6c865
Luan121314 <Luan.staner@gmail.com>
2019-04-05 20:33:19 -0300

    code incompleto, necessita trocar de maquina

---
+++ possodirigir/app/src/main/java/br/com/possodirigir/Launch.kt
@@ -0,0 +1,28 @@
+package br.com.possodirigir
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class Launch : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //antes produzir o conteudo da tela faça...
+        // deixando a tela fullscreen, sem barra de status
+
+
+        setContentView(R.layout.activity_launch)
+
+        //Passando para prox janela depois de um espaco de tempo
+        Handler().postDelayed({
+            val intentMain = Intent(this, MainActivity::class.java)
+
+            startActivity(intentMain)
+        },3000)
+
+    }
+}
